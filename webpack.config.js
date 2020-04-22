271256069db3a8fd38d0354c1ed27d87e32b8584
alsterium <new033@gmail.com>
2020-04-22 18:01:39 +0900

    add index.html

--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,21 @@ module.exports = {
   // ローカル開発用環境を立ち上げる
   // 実行時にブラウザが自動的に localhost を開く
   devServer: {
-    contentBase: "dist",
-    open: true
+    contentBase: "./",
+    open: true,
   },
+
+  //というかCDN分けるとwebpackのうまみ無くなるのでは？？？
+  // //reqire('three')で呼び出されるモジュールがグローバル変数のTHREEになる
+  // externals: {
+  //   three:'THREE'
+  // },
 
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
     path: `${__dirname}/dist`,
     // 出力ファイル名
-    filename: "main.js"
-  }
+    filename: "main.js",
+  },
 };
