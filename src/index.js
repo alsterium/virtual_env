ce8eba30b3f0e55fc4986bfd5cee92e68429edbb
alsterium <new033@gmail.com>
2020-04-28 16:58:28 +0900

    add floor and panel

--- src/index.js
+++ src/index.js
@@ -1,28 +1,50 @@
 let THREE = require('three');
+//orbitControl
+let Orbit = require('../node_modules/three/examples/jsm/controls/OrbitControls');
+
 
 (function(){
-    var scene,  camera, renderer;
-    var geometry, material, mesh;
+    var scene,  camera, renderer, controls;
+    var mesh, light, ambient;
 
     init();
     animate();
 
     function init() {
-
         scene = new THREE.Scene();
   
         camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-        camera.position.z = 1000;
-  
-        geometry = new THREE.BoxGeometry( 200, 200, 200 );
-        material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-  
-        mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
-  
+        camera.position.set(-5,1,0);
+        
+        ambient = new THREE.AmbientLight(0xFFFFFF,0.3);
+        light = new THREE.DirectionalLight(0xFFFFFF,0.8);
+
+        scene.add(ambient,light);
+        
+        //床の生成
+        let geometry = new THREE.BoxGeometry(2000,0.1,2000);
+        let material = new THREE.MeshStandardMaterial({color:0x708090});
+        
+        floor = new THREE.Mesh( geometry, material );
+        floor.position.set(0,-0.1,0)
+        scene.add( floor );
+
+        //パネルの生成
+        let geometry_P = new THREE.BoxGeometry(16,9,0.1);
+        let material_P = new THREE.MeshStandardMaterial({color:0xFF0000});
+        panel = new THREE.Mesh(geometry_P,material_P);
+        panel.position.set(20,9,0);
+        panel.rotation.y = 90/180*Math.PI;
+        scene.add(panel);
+        
+        let axesHelper = new THREE.AxesHelper();
+        scene.add(axesHelper);
         renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
-  
+
+        //カメラコントロール
+        controls = new Orbit.OrbitControls(camera, renderer.domElement);
+
         document.body.appendChild( renderer.domElement );
   
     }
@@ -30,10 +52,6 @@ let THREE = require('three');
     function animate() {
 
         requestAnimationFrame( animate );
-  
-        mesh.rotation.x += 0.01;
-        mesh.rotation.y += 0.02;
-  
         renderer.render( scene, camera );
   
     }
